5adc57bd91621fa3eaf8ddaead5a91e446c45f08
Chris Hill <cnh@mit.edu>
2020-08-25 15:15:52 -0400

    Update ivdc_diagnostics.jl

--- standalone-solver-testing/src/ivdc_diagnostics.jl
+++ standalone-solver-testing/src/ivdc_diagnostics.jl
@@ -5,6 +5,10 @@ end
 function ivdc_default_diagnostics_fini()
 end
 function ivdc_default_diagnostics_collect()
+    mpicomm = Diagnostics.Settings.mpicomm
+    dg1 = Diagnostics.Settings.dg.modeldata.diagsconf.myivdcdg
+    dg2 = Diagnostics.Settings.dg.modeldata.diagsconf.myivdcdg2
+    Q = Diagnostics.Settings.dg.modeldata.diagsconf.Q
 end
 
 function setup_single_column_default_diagnostics(::IVDCConfigType, interval, out_pref; writer=NetCDFWriter(),interpol=nothing)
