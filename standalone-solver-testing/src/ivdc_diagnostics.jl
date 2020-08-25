637afc07f8b113aedf477632b1024e69dc899308
christophernhill <cnh@mit.edu>
2020-08-24 22:40:43 -0400

    More diagnostics prep

--- standalone-solver-testing/src/ivdc_diagnostics.jl
+++ standalone-solver-testing/src/ivdc_diagnostics.jl
@@ -1,5 +1,23 @@
 using ClimateMachine.Writers
 
+function ivdc_default_diagnostics_init()
+end
+function ivdc_default_diagnostics_fini()
+end
+function ivdc_default_diagnostics_collect()
+end
+
 function setup_single_column_default_diagnostics(::IVDCConfigType, interval, out_pref; writer=NetCDFWriter(),interpol=nothing)
+
+	return DiagnosticsGroup(
+				"IVDCdefault",
+				ivdc_default_diagnostics_init,
+				ivdc_default_diagnostics_fini,
+				ivdc_default_diagnostics_collect,
+				interval,
+				out_pref,
+				writer,
+				interpol,
+				)
 
 end
